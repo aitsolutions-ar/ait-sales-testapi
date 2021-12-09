64af088853b416a981a4dd00e8055307c07dac2b
elianagarcia5 <90462155+elianagarcia5@users.noreply.github.com>
2021-12-09 12:06:13 -0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -5,6 +5,7 @@ products = [
         "description": "Est pariatur pariatur tempor minim est velit aute elit voluptate.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639058009/desafio%20front%20ait/shop-6-600x600_aza1vl.jpg",
         "price": "$278.91",
+        "stock": 8
     },
     {
         "_id": "61a917ed3b91c1fd79f432c3",
@@ -12,6 +13,7 @@ products = [
         "description": "Deserunt qui pariatur qui nisi cillum id est incididunt aliquip dolore incididunt.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639058042/desafio%20front%20ait/shop-8-600x600_sqnau2.jpg",
         "price": "$138.04",
+        "stock": 9
     },
     {
         "_id": "61a917eda7843136564629c2",
@@ -27,6 +29,7 @@ products = [
         "description": "Ea sit sunt deserunt sit veniam quis aliquip proident excepteur.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639058294/desafio%20front%20ait/muffin-1-600x600_irwmgq.jpg",
         "price": "$151.44",
+        "stock": 92
     },
     {
         "_id": "61a917ed8a3298f608331284",
@@ -34,6 +37,7 @@ products = [
         "description": "Do incididunt sunt amet cupidatat exercitation aliqua fugiat irure quis.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639057475/desafio%20front%20ait/5-1_fiox50.png",
         "price": "$261.45",
+        "stock": 0
     },
     {
         "_id": "61a917ed01488a1e62299bd1",
@@ -41,6 +45,7 @@ products = [
         "description": "Aute nostrud eiusmod amet quis velit duis mollit ex officia.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639056997/desafio%20front%20ait/cake-4-600x600_m6qxlr.jpg",
         "price": "$435.28",
+        "stock": 82
     },
     {
         "_id": "61a917ed00d4d6ed372e7a92",
@@ -48,6 +53,7 @@ products = [
         "description": "Voluptate reprehenderit exercitation ipsum non proident veniam consequat irure.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639057104/desafio%20front%20ait/danish-1-600x600_y1tf2h.jpg",
         "price": "$93.45",
+        "stock": 3
     },
     {
         "_id": "61a917ed5e61fd5a943db001",
@@ -55,6 +61,7 @@ products = [
         "description": "Nisi ea non do irure incididunt dolor exercitation cillum elit est ullamco sit.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639058352/desafio%20front%20ait/danish-2-600x600_udnjvq.jpg",
         "price": "$191.69",
+        "stock": 18
     },
     {
         "_id": "61a917ed5e61fd1a943dc086",
@@ -62,6 +69,7 @@ products = [
         "description": "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639058574/desafio%20front%20ait/muffin-2-600x600_iqzorx.jpg",
         "price": "$170",
+        "stock": 0
     },
     {
         "_id": "61a917ed5ewd7e45f448gwt1",
@@ -69,6 +77,7 @@ products = [
         "description": "Doloremque laudantium, totam rem aperiam.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639058938/desafio%20front%20ait/item_08-600x600_rzl7ke.png",
         "price": "$190.80",
+        "stock": 7
     },
     {
         "_id": "61a917ed5e263c4j2g6c3864",
@@ -76,6 +85,7 @@ products = [
         "description": "Inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639059307/desafio%20front%20ait/item_02-600x600_a1a3xq.png",
         "price": "$124.73",
+        "stock": 25
     },
     {
         "_id": "61a917ed5e4bj643hv4gf6j7",
@@ -83,6 +93,7 @@ products = [
         "description": "Donec tempor sem quis diam luctus euismod.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639060181/desafio%20front%20ait/product-11_rv1pjm.png",
         "price": "$495.90",
+        "stock": 13
     },
     {
         "_id": "61a917ed5e4v6jjb6cp3c4",
@@ -90,6 +101,7 @@ products = [
         "description": "Ut hendrerit lorem sed dui auctor, ac consequat nibh sollicitudin.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639060939/desafio%20front%20ait/pastry_transparent_02_qsh1el.png",
         "price": "$255.90",
+        "stock": 0
     },
     {
         "_id": "61a917ed5ejc8etsj9yn8tg1",
@@ -97,6 +109,7 @@ products = [
         "description": "Ut hendrerit lorem sed dui auctor, ac consequat nibh sollicitudin.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639061077/desafio%20front%20ait/pastry_transparent_13_hwdiq9.png",
         "price": "$465.18",
+        "stock": 3
     },
     {
         "_id": "61a917ed5e4bkvk3gv4g5jo2",
@@ -104,6 +117,7 @@ products = [
         "description": "Integer fermentum, lectus at luctus faucibus, metus lectus dapibus quam, euismod blandit nisi felis et libero.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639061384/desafio%20front%20ait/shop-img-2-600x600_oxae89.jpg",
         "price": "$188",
+        "stock": 1
     },
     {
         "_id": "61a917ed5efdji7s3gy43e25",
@@ -111,6 +125,7 @@ products = [
         "description": "Donec luctus, est a hendrerit porta, dui tellus dignissim libero.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639061992/desafio%20front%20ait/05-506x580_bckhm9.jpg",
         "price": "$204.88",
+        "stock": 46
     },
     {
         "_id": "61a917ed5hkw7gj4i1s3fe7",
@@ -118,5 +133,6 @@ products = [
         "description": "Curabitur a dolor eget nibh convallis convallis et vel turpis.",
         "image": "https://res.cloudinary.com/dfdphddxu/image/upload/v1639062028/desafio%20front%20ait/07-506x580_cyrgfk.jpg",
         "price": "$240",
+        "stock": 27
     }
 ]
